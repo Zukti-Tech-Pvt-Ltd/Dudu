b7fe62b9d033c497b0781962ceb58350fc6291b5
yukesh570 <yukesh.maha10@gmail.com>
2025-09-26 00:24:54 +0545

    chnages

--- src/api/khaltiApi.ts
+++ src/api/khaltiApi.ts
@@ -1,7 +1,15 @@
 import api from ".";
 
 
-export const khaltiPayment = async () => {
-    const response = await api.post('/api/khalti/initiate');
+export const khaltiPayment = async (
+    selectedItems: { id: string; quantity: number ,price: number}[],
+    userId?:number,
+    totalPrice?:number  
+) => {
+      const selectedItemsParam = encodeURIComponent(JSON.stringify(selectedItems));
+
+    console.log('========selectedItems', selectedItems);
+    console.log('=========userId', userId);
+    const response = await api.post(`/api/khalti/initiate/${userId}/${selectedItemsParam}/${totalPrice}`);
     return response.data;
 };
